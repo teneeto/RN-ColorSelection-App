1254add7ea469d20349b11d0fc87e562dfde67f5
Olei Eto <teneeto@gmail.com>
2020-08-04 01:22:28 +0100

    add navigation

--- App.js
+++ App.js
@@ -1,54 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, SafeAreaView, FlatList } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import ColorBox from './components/ColorBox';
+import Home from './screens/Home';
+import ColorPalette from './screens/ColorPallete';
 
-const COLORS = [
-  { colorName: 'Base03', hexCode: '#002b36' },
-  { colorName: 'Base02', hexCode: '#073642' },
-  { colorName: 'Base01', hexCode: '#586e75' },
-  { colorName: 'Base00', hexCode: '#657b83' },
-  { colorName: 'Base0', hexCode: '#839496' },
-  { colorName: 'Base1', hexCode: '#93a1a1' },
-  { colorName: 'Base2', hexCode: '#eee8d5' },
-  { colorName: 'Base3', hexCode: '#fdf6e3' },
-  { colorName: 'Yellow', hexCode: '#b58900' },
-  { colorName: 'Orange', hexCode: '#cb4b16' },
-  { colorName: 'Red', hexCode: '#dc322f' },
-  { colorName: 'Magenta', hexCode: '#d33682' },
-  { colorName: 'Violet', hexCode: '#6c71c4' },
-  { colorName: 'Blue', hexCode: '#268bd2' },
-  { colorName: 'Cyan', hexCode: '#2aa198' },
-  { colorName: 'Green', hexCode: '#859900' },
-];
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <SafeAreaView style={styles.areaView}>
-        <FlatList
-          style={styles.container}
-          data={COLORS}
-          keyExtractor={(item) => item.colorName}
-          renderItem={({ item }) => (
-            <ColorBox colorName={item.colorName} hexCode={item.hexCode} />
-          )}
-          ListHeaderComponent={<Text style={styles.heading}>Solarized</Text>}
-        />
-      </SafeAreaView>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="ColorPalette" component={ColorPalette} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    paddingTop: 50,
-    paddingHorizontal: 10,
-  },
-  heading: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 10,
-  },
-});
---
+++ screens/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+
+const Home = ({ navigation }) => {
+  return (
+    <View>
+      <TouchableOpacity onPress={() => navigation.navigate('ColorPalette')}>
+        <Text>Hello World</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default Home;
