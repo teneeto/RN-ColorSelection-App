bbdab521bd72de012893cd330bb5c6845b1c84ee
Olei Eto <teneeto@gmail.com>
2020-08-06 16:41:05 +0100

    add textInput and alert to ColorPaletteModal

--- screens/ColorPaletteModal.js
+++ screens/ColorPaletteModal.js
@@ -1,12 +1,63 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useCallback } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
 
 const ColorPaletteModal = () => {
+  const [name, setName] = useState('');
+
+  const handleSubmit = useCallback(() => {
+    if (!name) {
+      Alert.alert('Please enter a palette name');
+    }
+  }, [name]);
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.container}>
+      <Text style={styles.name}>Name of the palette</Text>
+      <TextInput
+        style={styles.input}
+        value={name}
+        onChangeText={setName}
+        placeholder="Palette name"
+      />
+      <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+        <Text style={styles.buttonText}>Submit</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  input: {
+    borderColor: 'grey',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 50,
+  },
+  container: {
+    padding: 10,
+    backgroundColor: 'white',
+    flex: 1,
+  },
+  button: {
+    height: 40,
+    backgroundColor: 'teal',
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  name: {
+    marginBottom: 10,
+  },
+});
 export default ColorPaletteModal;
